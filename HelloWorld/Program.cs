84253b9b6d042e48c4699a7c4eda4311590b0d42
Veronique Zimmermann <zimmermannveronique@protonmail.com>
2017-10-09 10:50:52 +0200

    Initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace test1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();     // wartet auf ein Enter
+        }
+    }
+}
